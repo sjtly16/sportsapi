aef792d76f181feee0b8651c60250fde28969b36
sjtly16 <sjtly16@gmail.com>
2021-10-10 20:30:52 +0530

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :matches, only: %i[index show]
-      resources :teams, only: show
-      resources :match_stat, only: show
+      resources :teams, only: [:show]
+      resources :match_stat, only: [:show]
     end
   end
 end
