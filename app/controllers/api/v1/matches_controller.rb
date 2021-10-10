9025ba8031d1ffec5f0e3ffd71b8cc5ac8c7353b
sjtly16 <sjtly16@gmail.com>
2021-10-10 22:44:27 +0530

    fix controller response

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::MatchesController < ApplicationController
     def index
-        render json: Match.find(params[:date])
+        """Upcoming Match List API"""
+        render json: {
+            data: Match.where('date > ?', Date.today), 
+            message: "Upcoming Match List",
+        }
     end
 
     def show
+        """Match Detail API"""
         match = Match.find(params[:id])
         match_id = match.id
         match_participants = MatchParticipant.find(match_id)
@@ -12,6 +17,13 @@ class Api::V1::MatchesController < ApplicationController
         host_team = Team.find(host_team_id)
         away_team = Team.find(away_team_id)
 
-        render json: {match: match, host_team: host_team, away_team: away_team}
+        render json: {
+            data: {
+                    match: match, 
+                    host_team: host_team, 
+                    away_team: away_team
+                },
+            message: "Match Detail"
+        }
     end
 end
