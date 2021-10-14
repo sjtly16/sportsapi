d5fa2a319ca1f6b0b27372820a7499b5de178a1f
sjtly16 <sjtly16@gmail.com>
2021-10-14 13:30:00 +0530

    add match stat between teams routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :matches, only: %i[index show]
       resources :teams, only: [:show]
-      resources :match_stat, only: [:show]
+      resources :match_stat do
+        collection do
+          get 'match_between_team'
+        end
+      end
     end
   end
 end
